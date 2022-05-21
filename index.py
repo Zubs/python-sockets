ea6a3cc5c152b41aae5b3c87eb6630bc6a1b1241
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-05-21 16:02:04 +0100

    Server setup

--- index.py
+++ index.py
@@ -1,6 +1,19 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Socket Setup
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = 3000
 hostname = '127.0.0.1' # Host IP
-s.bind((hostname, port))
+serverSocket.bind((hostname, port))
+print('Listening at {}'.format(serverSocket.getsockname()))
+
+def processInput():
+    data, clientAddress = serverSocket.recvfrom(65535)
+    message = data.decode('ascii')
+    uppercaseMessage = message.upper()
+    data = uppercaseMessage.encode('ascii')
+    print('The client at {} says {!r}'.format(clientAddress, message))
+    serverSocket.sendto(data, clientAddress)
+
+while True:
+    processInput();
