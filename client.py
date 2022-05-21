07091ecc6d21e8581abcd3688d8663db61835cad
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-05-21 16:20:36 +0100

    Connection security

--- client.py
+++ client.py
@@ -2,10 +2,11 @@ import socket
 
 # Socket setup
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSocket.connect(('127.0.0.1', 3000))
 
 message = input("Enter a sentence: ")
 data = message.encode('ascii')
-clientSocket.sendto(data, ('127.0.0.1', 3000))
+clientSocket.send(data)
 data, address = clientSocket.recvfrom(65535)
 text = data.decode('ascii')
 print('The server, {}, responded with {!r}'.format(address, text))
